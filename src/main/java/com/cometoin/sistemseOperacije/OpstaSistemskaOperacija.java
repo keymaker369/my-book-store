2ade3b6fc4045d6853a2f42007ad491cd9690afb
unknown <nenad.seke@pc-nenads.kc.visioninc.com>
2016-10-06 17:52:06 +0200

    something

--- src/main/java/com/cometoin/sistemseOperacije/OpstaSistemskaOperacija.java
+++ src/main/java/com/cometoin/sistemseOperacije/OpstaSistemskaOperacija.java
@@ -4,8 +4,16 @@
  */
 package com.cometoin.sistemseOperacije;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.model.naming.ImplicitNamingStrategyJpaCompliantImpl;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
 import com.cometoin.dbbr.DatabaseBroker;
 import com.cometoin.domenskeKlase.OpstiDomenskiObjekat;
+import com.cometoin.domenskeKlase.Proizvod;
 
 /**
  *
@@ -37,6 +45,25 @@ public abstract class OpstaSistemskaOperacija {
 
     private void otvoriBazu() {
         signal = DatabaseBroker.getInstance().poveziSeSaBazom();
+        
+        //http://docs.jboss.org/hibernate/orm/5.2/topical/html_single/bootstrap/NativeBootstrapping.html
+        StandardServiceRegistry standardRegistry = new StandardServiceRegistryBuilder()
+        	.configure( "org/hibernate/example/MyCfg.xml" )
+        	.build();
+
+		Metadata metadata = new MetadataSources( standardRegistry )
+		        .addAnnotatedClass( Proizvod.class )
+		        .addAnnotatedClassName( "org.hibernate.example.Customer" )
+		        .addResource( "org/hibernate/example/Order.hbm.xml" )
+		        .addResource( "org/hibernate/example/Product.orm.xml" )
+		        .getMetadataBuilder()
+		        .applyImplicitNamingStrategy( ImplicitNamingStrategyJpaCompliantImpl.INSTANCE )
+		        .build();
+		
+//		SessionFactory sessionFactory = metadata.getSessionFactoryBuilder()
+//		        .applyBeanManager( getBeanManagerFromSomewhere() )
+//		        .build();
+        
     }
 
     public boolean proveriUspesnostOpsta(int sig) {
